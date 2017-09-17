489cbc36fcce94423f6acb9263c353ad42cbbf73
Adam Chodorowski <adam@ufs.se>
2017-09-17 22:22:13 +0200

    Add some tests.

---
+++ test_census.py
@@ -0,0 +1,87 @@
+#!/bin/env python3
+import unittest
+from census import *
+
+class TestFunctions(unittest.TestCase):
+    def test_parse_gender(self):
+        self.assertEqual('K', parse_gender('K'))
+        self.assertEqual('K', parse_gender('Kvinna'))
+        self.assertEqual('K', parse_gender('TJEJ'))
+        self.assertEqual('K', parse_gender('female'))
+
+        self.assertEqual('M', parse_gender('M'))
+        self.assertEqual('M', parse_gender('Man'))
+        self.assertEqual('M', parse_gender('Male'))
+        self.assertEqual('M', parse_gender('kille'))
+
+        self.assertEqual(None, parse_gender(''))
+        self.assertEqual(None, parse_gender('?'))
+        self.assertEqual(None, parse_gender('ej svar'))
+        self.assertEqual(None, parse_gender('ANNAT'))
+
+        self.assertRaisesRegex(Exception, 'invalid gender.*', parse_gender, 'WAT')
+
+    def test_parse_year(self):
+        self.assertEqual('1997', parse_year('1997'))
+        self.assertEqual('1951', parse_year('51'))
+        self.assertEqual('2050', parse_year('50'))
+        self.assertEqual('1997', parse_year('97'))
+
+    def test_parse_date(self):
+        self.assertEqual(None, parse_date(''))
+        self.assertEqual(('2017', '10', '23'), parse_date('17.10.23'))
+        self.assertEqual(('2017', '10', '23'), parse_date('2017.10.23'))
+        self.assertEqual(('2017', '10', '23'), parse_date('2017-10-23'))
+        self.assertEqual(('2017', '10', '23'), parse_date('2017/10/23'))
+        self.assertEqual(('2017', '10', '23'), parse_date('2017/10/23'))
+        self.assertRaises(Exception, parse_date, '2017-07:23')
+        
+    def test_format_date(self):
+        self.assertEqual('', format_date(None))
+        self.assertEqual('2017-07-14', format_date(('2017', '07', '14')))
+
+    def test_parse_birth_date(self):
+        # Invalid
+        self.assertRaises(Exception, parse_birth_date, 'WAT')
+        self.assertRaises(Exception, parse_birth_date, '2015-10-TT')
+
+        # None
+        self.assertEqual(None, parse_birth_date(''))
+        self.assertEqual(None, parse_birth_date('?'))
+        self.assertEqual(None, parse_birth_date('0'))
+        
+        # Only year
+        self.assertEqual(('1995', None, None, None), parse_birth_date('1995'))
+        self.assertEqual(('1995', None, None, None), parse_birth_date('95'))
+        self.assertEqual(('2015', None, None, None), parse_birth_date('15'))
+
+        # Date
+        self.assertEqual(('1995', '10', '14', None), parse_birth_date('1995-10-14'))
+        self.assertEqual(('1995', '10', '14', None), parse_birth_date('1995/10/14'))
+        self.assertEqual(('1995', '10', '14', None), parse_birth_date('19951014'))
+        self.assertEqual(('1995', '10', '14', None), parse_birth_date('95-10-14'))
+        self.assertEqual(('1995', '10', '14', None), parse_birth_date('95/10/14'))
+        self.assertEqual(('1995', '10', '14', None), parse_birth_date('951014'))
+        self.assertEqual(('2015', '10', '14', None), parse_birth_date('151014'))
+
+        # SSN
+        self.assertEqual(('1995', '10', '14', '3856'), parse_birth_date('1995-10-14-3856'))
+
+    def test_format_birth_date(self):
+        self.assertEqual('', format_birth_date(None))
+        self.assertEqual('1995', format_birth_date(('1995', None, None, None)))
+        self.assertEqual('1995-10-14', format_birth_date(('1995', '10', '14', None)))
+        self.assertEqual('1995-10-14-3856', format_birth_date(('1995', '10', '14', '3856')))
+    
+    def test_parse_groups(self):
+        self.assertEqual([], parse_groups(None))
+        self.assertEqual(['A'], parse_groups('A'))
+        self.assertEqual(['A', 'B', 'C'], parse_groups('A;B;C'))
+
+    def test_format_groups(self):
+        self.assertEqual('', format_groups([]))
+        self.assertEqual('A', format_groups(['A']))
+        self.assertEqual('A;B;C', format_groups(['A', 'B', 'C']))
+
+if __name__ == '__main__':
+    unittest.main()
